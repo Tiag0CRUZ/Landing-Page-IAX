84023feb52cefc39612aed97daf92412feb0dded
Tiago Cruz <tiago_flc@hotmail.com>
2026-03-17 20:38:09 -0400

    Add files via upload

---
+++ scriptEventos.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', () => {
+    lucide.createIcons();
+    const sections = document.querySelectorAll('section');
+    const options = {
+        threshold: 0.1
+    };
+
+    const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.style.opacity = '1';
+                entry.target.style.transform = 'translateY(0)';
+                observer.unobserve(entry.target);
+            }
+        });
+    }, options);
+
+    sections.forEach(section => {
+        section.style.opacity = '0';
+        section.style.transform = 'translateY(20px)';
+        section.style.transition = 'opacity 0.6s ease-out, transform 0.6s ease-out';
+        observer.observe(section);
+    });
+});
